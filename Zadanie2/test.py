29589846d666fbdfa917185289572a89690555b5
Karol Kazusek <karol@localhost.localdomain>
2024-11-09 11:28:29 +0100

    test.py

--- Zadanie2/test.py
+++ Zadanie2/test.py
@@ -18,6 +18,10 @@ parameters = {'n_neighbors':[2, 4, 8]}
 clf = GridSearchCV(KNeighborsClassifier(metric=DTW), parameters, cv=3, verbose=1)
 clf.fit(X_train, y_train)
 
+#evaluate
+y_pred = clf.predict(X_test)
+print(classification_report(y_test, y_pred))
+
 def dtw_kernel(series_1, series_2):
     """
     Funkcja jądra oparta na metryce DTW dla SVM.
@@ -34,6 +38,21 @@ def dtw_kernel(series_1, series_2):
     
     return kernel_matrix
 
-#evaluate
-y_pred = clf.predict(X_test)
-print(classification_report(y_test, y_pred))
+
+# Przykładowe dane: X_train - treningowe, X_test - testowe, y_train - etykiety treningowe
+X_train = [np.random.rand(50) for _ in range(10)]  # np. 10 sekwencji o długości 50
+y_train = np.random.choice([0, 1], size=10)  # Etykiety binarne
+X_test = [np.random.rand(50) for _ in range(5)]  # np. 5 sekwencji do testowania
+
+# Tworzymy model SVM z naszym jądrem DTW
+svm_dtw = SVC(kernel="precomputed")
+
+# Obliczamy macierz jądra dla danych treningowych
+K_train = dtw_kernel(np.array(X_train), np.array(X_train))
+svm_dtw.fit(K_train, y_train)
+
+# Obliczamy macierz jądra dla danych testowych
+K_test = dtw_kernel(np.array(X_test), np.array(X_train))
+y_pred = svm_dtw.predict(K_test)
+
+print("Predykcje na danych testowych:", y_pred)
